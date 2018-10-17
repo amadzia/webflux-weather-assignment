bcf63807fead75408e8eef70d2aacccc6588f817
amadzia <agnieszkamadzia@onet.pl>
2018-10-17 08:40:42 +0200

    first commit

---
+++ src/main/java/com/example/webfluxweatherassignment/WebfluxWeatherAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.example.webfluxweatherassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxWeatherAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxWeatherAssignmentApplication.class, args);
+    }
+}
