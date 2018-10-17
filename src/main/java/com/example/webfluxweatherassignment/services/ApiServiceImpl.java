11586cfd7bf3df98535b4b9b163e34ad032c8c71
amadzia <agnieszkamadzia@onet.pl>
2018-10-17 10:33:41 +0200

    Refactor ApiServiceImpl

--- src/main/java/com/example/webfluxweatherassignment/services/ApiServiceImpl.java
+++ src/main/java/com/example/webfluxweatherassignment/services/ApiServiceImpl.java
@@ -2,8 +2,11 @@ package com.example.webfluxweatherassignment.services;
 
 import com.example.api.v1.domain.WeatherData;
 import com.example.api.v1.domain.WeatherForecast;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -16,16 +19,25 @@ public class ApiServiceImpl implements ApiService {
     private RestTemplate restTemplate;
     private WeatherData weatherData;
     private WeatherForecast weatherForecast;
+    private final String api_url;
+    private final String api_key;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url, @Value("${api.key}") String api_key) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
+        this.api_key = api_key;
     }
 
     @Override
     public WeatherForecast getWeatherForecast(String cityName, String countryCode) {
 
-        weatherData = restTemplate.getForObject("https://samples.openweathermap.org/data/2.5/forecast?q="
-                + cityName + "," + countryCode + "&appid=b6907d289e10d714a6e88b30761fae22", WeatherData.class);
+        UriComponents uriComponent = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("q", "{value}")
+                .queryParam("appid", "{value}").build()
+                .expand(cityName + "," + countryCode, api_key).encode();
+
+        weatherData = restTemplate.getForObject(uriComponent.toString(), WeatherData.class);
 
         weatherForecast = new WeatherForecast();
 
